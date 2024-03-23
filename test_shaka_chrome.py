64430d25be28d5aaa044994e10cef2af2c95d4ed
Gil <ggonen@synamedia.com>
2024-03-24 00:07:41 +0200

    test shaka playback

--- test_shaka_chrome.py
+++ test_shaka_chrome.py
@@ -1,3 +1,4 @@
+import time
 from seleniumbase import BaseCase
 BaseCase.main(__name__, __file__)
 
@@ -9,3 +10,29 @@ class TestShakaBrowser(BaseCase):
         txt = self.get_text_content("#output")
         print(txt)
         assert('"com.widevine.alpha": {\n      "persistentState":' in txt)
+
+    def test_playback(self):
+        # clear asset
+        #url = "https://harmonicinc-com.github.io/shaka-player/latest/demo/#audiolang=en-US;textlang=en-US;uilang=en-US;panel=HOME;build=debug_compiled"
+        # asset encrypted with widevine
+        url = "https://harmonicinc-com.github.io/shaka-player/latest/demo/#audiolang=en-US;textlang=en-US;uilang=en-US;panel=ALL_CONTENT;panelData=source:SHAKA,drm:WIDEVINE,MP4,DASH,VOD;build=debug_compiled"
+        self.open(url)
+
+        self.wait_for_element('//button[text()="Play"]', by='xpath')
+        self.click('//button[text()="Play"]', by='xpath')
+
+        videoEl = self.wait_for_element("video#video",timeout=4)
+
+        readyState = videoEl.get_attribute("readyState")
+        #self.assert_equal(readyState, "0", msg=None)
+
+        self.wait_for_attribute("video#video", "readyState", "4", timeout=4)
+
+        src = videoEl.get_attribute("src")
+        self.console_log_string("playing src: " + src)
+
+        secondsToPlay = 2
+        time.sleep(secondsToPlay + 1)
+
+        currentTime = int(float(self.get_attribute("video#video", "currentTime")))
+        self.assertTrue(currentTime >= secondsToPlay, msg=None)
